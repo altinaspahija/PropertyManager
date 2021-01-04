e01b5e7ca4aa37443af07b74bb98669b53c5036a
altinaspahija <altinaspahija@hotmail.com>
2021-01-04 22:19:52 +0100

    Add files via upload

--- src/main/java/net/codejava/PropertyRepository.java
+++ src/main/java/net/codejava/PropertyRepository.java
@@ -1,2 +1,15 @@
-package net.codejava;public interface PropertyRepository {
+package net.codejava;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PropertyRepository extends JpaRepository<Property, Integer> {
+
+    @Query("SELECT p FROM Property p WHERE p.user_id = :user_id")
+
+    public List<Property> getPropertiesByUser_id (@Param("user_id") Integer user_id);
+
 }
--- src/main/java/net/codejava/PropertyService.java
+++ src/main/java/net/codejava/PropertyService.java
@@ -1,2 +1,32 @@
-package net.codejava;public class PropertyService {
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PropertyService {
+    @Autowired
+    private PropertyRepository repo;
+
+    public List<Property> listAll() {
+        return repo.findAll();
+    }
+
+    public List<Property> listAllByUserId(Integer userId) {
+        return repo.getPropertiesByUser_id(userId);
+    }
+
+    public void save(Property property) {
+        repo.save(property);
+    }
+
+    public Property get(Integer property_id) {
+        return repo.findById(property_id).get();
+    }
+
+    public void delete(Integer property_id) {
+        repo.deleteById(property_id);
+    }
 }
