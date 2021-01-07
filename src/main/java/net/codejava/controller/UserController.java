f7d931d07a9174f03c98218d3e575ce156e96439
altinaspahija <altinaspahija@hotmail.com>
2021-01-08 00:38:24 +0100

    Add files via upload

---
+++ src/main/java/net/codejava/controller/UserController.java
@@ -0,0 +1,37 @@
+package net.codejava.controller;
+
+import net.codejava.dto.PropertyDto;
+import net.codejava.dto.UserDto;
+import net.codejava.repository.PropertyRepository;
+import net.codejava.repository.UserRepository;
+import net.codejava.service.PropertyService;
+import net.codejava.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<UserDto> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserDto getUsersByUserId(@PathVariable Integer userId) {
+       UserDto user = userService.getUserByUserId(userId);
+       return user;
+    }
+
+}
