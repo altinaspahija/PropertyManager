035773000ea81c703e4d0e3d7926d84e9c05935a
altinaspahija <altinaspahija@hotmail.com>
2021-01-12 15:17:24 +0100

    Update WebSecurityConfig.java

--- src/main/java/net/codejava/login/WebSecurityConfig.java
+++ src/main/java/net/codejava/login/WebSecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
-
+/*
 //@Configuration
 //@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
